c24937eed1071e853054a97dcfdfebde28840ff5
martin-creator <martinlubowa@outlook.com>
2023-08-14 19:48:35 +0300

    Successfully implemented a graph alogorithm

--- breadth_search.py
+++ breadth_search.py
@@ -42,4 +42,41 @@
 # Dequeue removes an item from the front of the queue.
 # There is no random access in a queue.
 
-# Imp
+# Implementation of breadth first search in Python
+
+# Import the deque class from the collections module
+from collections import deque
+from re import search
+
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+
+def person_is_seller(name):
+    return name[-1] == 'm' # Check whether the person's name ends with 'm'. If it does, they're a mango seller
+
+def search(name):
+    search_queue = deque() # Create a queue
+    search_queue += graph[name] # Add all of your neighbors to the search queue
+    searched = [] # This array is how you keep track of which people you've searched before.
+    while search_queue: # While the queue isn't empty
+        person = search_queue.popleft() # Grab the first person off the queue
+        if not person in searched: # Only search this person if you haven't already searched them
+            if person_is_seller(person): # Check whether the person is a mango seller
+                print(person + " is a mango seller!") # Yes, they're a mango seller
+                return True
+            else:
+                search_queue += graph[person]
+                # No, they aren't. Add all of this person's friends to the search queue
+                searched.append(person) # Marks this person as searched
+    return False # If you reached here, no one in the queue was a mango seller
+
+print(search("you")) # Output: thom is a mango seller! True
+
